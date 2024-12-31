184f79d01880b8c9f673d3c0977dd091055e535c
vanshikaChhikaraGit <vanshikachhikaraofficial@gmail.com>
2025-01-01 02:58:12 +0530

    resolved signin button issue

--- frontend/components/shared/header.tsx
+++ frontend/components/shared/header.tsx
@@ -6,15 +6,15 @@ export default function Header(){
     return <header className="max-w-screen border-b flex">
         <div className="wrapper flex items-center justify-between">
             <Link href={"/"}>
-           <span className="font-light text-xl md:text-3xl"> IGDTUW Events </span> 
+           <span className="font-light text-xl md:text-3xl z-21"> IGDTUW Events </span> 
              </Link>
        
         <div className="flex justify-end items-center gap-2">
             <SignedIn >
-                <UserButton afterSignOutUrl='/'></UserButton>
+                <UserButton afterSignOutUrl="/"></UserButton>
             </SignedIn>
             <SignedOut>
-               <div className="bg-gradient-to-r from-blue-500 to-blue-700 font-medium text-white py-2 px-4 rounded-full shadow-lg ">
+               <div className="bg-gradient-to-r z-20 from-blue-500 to-blue-700 font-medium text-white py-2 px-4 rounded-full shadow-lg ">
                 <SignInButton mode="modal" ></SignInButton>
                 </div>
              </SignedOut>
