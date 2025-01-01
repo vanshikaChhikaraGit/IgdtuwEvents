db9573247bd8f556b259490f6b5ebb3284556114
boredHomosapien <vanshikachhikaraofficial@gmail.com>
2025-01-01 19:55:10 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
-  
-};
-module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  
 };
 
 export default nextConfig;
